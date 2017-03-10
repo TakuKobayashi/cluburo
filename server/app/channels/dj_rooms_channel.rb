4c6ea10a57d44edc5b8e6f6ab09e2a5b5618578d
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-03-10 14:45:31 +0900

    dj_roomsのchannelを作った

---
+++ server/app/channels/dj_rooms_channel.rb
@@ -0,0 +1,12 @@
+class DjRoomsChannel < ApplicationCable::Channel
+  def subscribed
+    # stream_from "some_channel"
+  end
+
+  def unsubscribed
+    # Any cleanup needed when channel is unsubscribed
+  end
+
+  def play
+  end
+end
