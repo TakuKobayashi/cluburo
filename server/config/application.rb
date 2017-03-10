122e75eac1a9697056143240723d5ef5f8aa630d
TakuKobayshi <takukobayshi@TakuKobayshis-iMac.local>
2017-03-10 11:01:35 +0900

    databaseを使わないでもrailsのアプリケーションを実行したかったのでそのための設定

--- server/config/application.rb
+++ server/config/application.rb
@@ -1,6 +1,16 @@
 require_relative 'boot'
 
-require 'rails/all'
+# DB を使わないので一旦コメントアウト
+#require 'rails/all'
+
+require "rails"      
+require "active_job/railtie"        
+require "action_controller/railtie"     
+require "action_mailer/railtie"     
+require "action_view/railtie"       
+require "action_cable/engine"       
+require "sprockets/railtie"       
+require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
