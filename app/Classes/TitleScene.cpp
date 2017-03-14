7ee2ee4a378309b105bd945cc60715a5428ed8c0
TakuKobayashi <keep_slimbody@yahoo.co.jp>
2017-03-15 03:25:59 +0900

    cocos2dxでデザイン組むとこんな感じなのかな?

--- app/Classes/TitleScene.cpp
+++ app/Classes/TitleScene.cpp
@@ -42,6 +42,36 @@ bool TitleScene::init()
     auto visibleSize = Director::getInstance()->getVisibleSize();
     Vec2 origin = Director::getInstance()->getVisibleOrigin();
     
+    auto background_sprite = Sprite::create("startscene/back_color_black.png");
+    background_sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    this->addChild(background_sprite, 0);
+    
+    auto bgstart_sprite = Sprite::create("startscene/background_start.png");
+    bgstart_sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    this->addChild(bgstart_sprite, 1);
+
+    auto logo_sprite = Sprite::create("startscene/logo.png");
+    logo_sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height - 160 + origin.y));
+    this->addChild(logo_sprite, 1);
+
+    auto icon_sprite = Sprite::create("startscene/icon.png");
+    icon_sprite->setPosition(Vec2(visibleSize.width/2 + origin.x, 230 + origin.y));
+    this->addChild(icon_sprite, 1);
+    
+    auto start_button = Sprite::create("startscene/button_start.png");
+    auto start_button_menu = MenuItemSprite::create(start_button, start_button, [this](Ref* sender){
+        //シーン移動
+        Director::getInstance()->replaceScene(HelloWorld::createScene());
+    });
+    //メニューの作成　pMenuの中にpBtnItemを入れる
+    auto menu = Menu::create(start_button_menu, NULL);
+    
+    //pMenuを画面中央に配置
+    menu->setPosition(Vec2(visibleSize.width/2 + origin.x, visibleSize.height/2 + origin.y));
+    this->addChild(menu, 2);
+
+    
+/*
     std::string fullPath = FileUtils::getInstance()->fullPathForFilename("MainScene.csb");
     log("--------------------------------------------------%s", fullPath.c_str());
     auto mainScene = CSLoader::getInstance()->createNode("MainScene.csb");
@@ -56,6 +86,7 @@ bool TitleScene::init()
         //シーン移動
         Director::getInstance()->replaceScene(HelloWorld::createScene());
     });
+ */
 
     /////////////////////////////
     // 2. add a menu item with "X" image, which is clicked to quit the program
@@ -66,6 +97,5 @@ bool TitleScene::init()
     
     // add a label shows "Hello World"
     // create and initialize a label
-    scheduleUpdate();
     return true;
 }
